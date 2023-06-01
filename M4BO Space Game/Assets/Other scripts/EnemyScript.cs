9ad1e55599132078331153298a7657ba2d30e19e
Clement <114576100+clem456@users.noreply.github.com>
2023-06-01 11:33:22 +0200

    Enemy Movement added

--- M4BO Space Game/Assets/Other scripts/EnemyScript.cs
+++ M4BO Space Game/Assets/Other scripts/EnemyScript.cs
@@ -1,25 +1,30 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEditor;
+using UnityEditor.Animations;
 using UnityEngine;
 
 public class EnemyScript : MonoBehaviour
 {
     public float speed = 10f;
+
     public float range = 20;
-    public float damageRange = 40;
+    public float attackRange = 10;
+
 
     public bool canSee = false;
 
     public GameObject target;
     
     private GameObject enemy;
+    private Animator animator;
 
     // Start is called before the first frame update
     void Start()
     {
         enemy = gameObject;
         enemy.tag = "Enemy";
+        animator = transform.GetComponentInChildren<Animator>();
     }
 
     // Update is called once per frame
@@ -29,10 +34,23 @@ public class EnemyScript : MonoBehaviour
 
         Vector3 lookAtPos = new Vector3(target.transform.position.x, transform.position.y, target.transform.position.z);
 
-        if (distance <= range)
+        if (distance <= range && distance > attackRange)
         {
+            Debug.Log("target is in range!");
             enemy.transform.LookAt(lookAtPos);
             enemy.transform.position += speed * Time.deltaTime * enemy.transform.forward;
+            animator.SetTrigger("Running");
+        }
+        else if (distance <= attackRange)
+        {
+            Debug.Log("target is in attack Range");
+            enemy.transform.LookAt(lookAtPos);
+            animator.SetTrigger("Attack");
+        }
+        else
+        {
+            Debug.Log("target is out of range!");
+            animator.SetTrigger("Idling");
         }
         /*
         else if (canSee && distance <= damageRange)
